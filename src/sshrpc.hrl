4cbf1cc14b591410626314a9afca2fb217d5007d
Kenji Rikitake <kenji.rikitake@acm.org>
2010-02-25 22:49:52 +0859

    added email address and EPL to sshrpc.hrl

--- src/sshrpc.hrl
+++ src/sshrpc.hrl
@@ -1,24 +1,23 @@
-%% sshrpc definitions
-
-%% basic binary constructor
-%% see ssh.hrl
--define(UINT32(X), X:32/unsigned-big-integer).
-
+%% Copyright (c) 2009-2010 Kenji Rikitake. All Rights Reserved.
 %%
-%% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 2004-2009. All Rights Reserved.
-%% 
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
 %% retrieved online at http://www.erlang.org/.
-%% 
+%%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
-%% 
-%% %CopyrightEnd%
-%%
+
+%% @author Kenji Rikitake <kenji.rikitake@acm.org>
+%% @copyright 2009-2010 Kenji Rikitake
+
+%% @doc sshrpc definitions
+
+%% basic binary constructor
+%% see ssh.hrl in Erlang/OTP
+-define(UINT32(X), X:32/unsigned-big-integer).
+
+%% end of file
