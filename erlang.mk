8beae7becf65d948a3781b9c6d845e77e0f5f8de
Kenji Rikitake <kenji.rikitake@acm.org>
2014-08-25 00:12:48 +0900

    Update erlang.mk

--- erlang.mk
+++ erlang.mk
@@ -115,6 +115,7 @@ define dep_fetch
 		git clone -n -- $$$$REPO $(DEPS_DIR)/$(1); \
 		cd $(DEPS_DIR)/$(1) && git checkout -q $$$$COMMIT; \
 	else \
+		echo "Unknown or invalid dependency: $(1). Please consult the erlang.mk README for instructions." >&2; \
 		exit 78; \
 	fi
 endef
@@ -124,7 +125,7 @@ $(DEPS_DIR)/$(1):
 	@mkdir -p $(DEPS_DIR)
 	@if [ ! -f $(PKG_FILE2) ]; then $(call core_http_get,$(PKG_FILE2),$(PKG_FILE_URL)); fi
 ifeq (,$(dep_$(1)))
-	DEPPKG=$$$$(awk 'BEGIN { FS = "\t" }; $$$$1 == "$(1)" { print $$$$2 " " $$$$3 " " $$$$4 }' $(PKG_FILE2);) \
+	DEPPKG=$$$$(awk 'BEGIN { FS = "\t" }; $$$$1 == "$(1)" { print $$$$2 " " $$$$3 " " $$$$4 }' $(PKG_FILE2);); \
 	VS=$$$$(echo $$$$DEPPKG | cut -d " " -f1); \
 	REPO=$$$$(echo $$$$DEPPKG | cut -d " " -f2); \
 	COMMIT=$$$$(echo $$$$DEPPKG | cut -d " " -f3); \
@@ -200,9 +201,13 @@ xyrl_verbose = $(xyrl_verbose_$(V))
 
 # Core targets.
 
-app:: ebin/$(PROJECT).app
+app:: erlc-include ebin/$(PROJECT).app
 	$(eval MODULES := $(shell find ebin -type f -name \*.beam \
 		| sed "s/ebin\//'/;s/\.beam/',/" | sed '$$s/.$$//'))
+	@if [ -z "$$(grep -E '^[^%]*{modules,' src/$(PROJECT).app.src)" ]; then \
+		echo "Empty modules entry not found in $(PROJECT).app.src. Please consult the erlang.mk README for instructions." >&2; \
+		exit 1; \
+	fi
 	$(appsrc_verbose) cat src/$(PROJECT).app.src \
 		| sed "s/{modules,[[:space:]]*\[\]}/{modules, \[$(MODULES)\]}/" \
 		> ebin/$(PROJECT).app
@@ -235,284 +240,13 @@ clean:: clean-app
 
 # Extra targets.
 
+erlc-include:
+	-@if [ -d ebin/ ]; then \
+		find include/ src/ -type f -name \*.hrl -newer ebin -exec touch $(shell find src/ -type f -name "*.erl") \; 2>/dev/null || echo -n; \
+	fi
+
 clean-app:
 	$(gen_verbose) rm -rf ebin/
-
-# Copyright (c) 2014, Loïc Hoguin <essen@ninenines.eu>
-# This file is part of erlang.mk and subject to the terms of the ISC License.
-
-.PHONY: bootstrap bootstrap-lib bootstrap-rel new list-templates
-
-# Core targets.
-
-help::
-	@printf "%s\n" "" \
-		"Bootstrap targets:" \
-		"  bootstrap          Generate a skeleton of an OTP application" \
-		"  bootstrap-lib      Generate a skeleton of an OTP library" \
-		"  bootstrap-rel      Generate the files needed to build a release" \
-		"  new t=TPL n=NAME   Generate a module NAME based on the template TPL" \
-		"  list-templates     List available templates"
-
-# Bootstrap templates.
-
-bs_appsrc = "{application, $(PROJECT), [" \
-	"	{description, \"\"}," \
-	"	{vsn, \"0.1.0\"}," \
-	"	{modules, []}," \
-	"	{registered, []}," \
-	"	{applications, [" \
-	"		kernel," \
-	"		stdlib" \
-	"	]}," \
-	"	{mod, {$(PROJECT)_app, []}}," \
-	"	{env, []}" \
-	"]}."
-bs_appsrc_lib = "{application, $(PROJECT), [" \
-	"	{description, \"\"}," \
-	"	{vsn, \"0.1.0\"}," \
-	"	{modules, []}," \
-	"	{registered, []}," \
-	"	{applications, [" \
-	"		kernel," \
-	"		stdlib" \
-	"	]}" \
-	"]}."
-bs_Makefile = "PROJECT = $(PROJECT)" \
-	"include erlang.mk"
-bs_app = "-module($(PROJECT)_app)." \
-	"-behaviour(application)." \
-	"" \
-	"-export([start/2])." \
-	"-export([stop/1])." \
-	"" \
-	"start(_Type, _Args) ->" \
-	"	$(PROJECT)_sup:start_link()." \
-	"" \
-	"stop(_State) ->" \
-	"	ok."
-bs_relx_config = "{release, {$(PROJECT)_release, \"1\"}, [$(PROJECT)]}." \
-	"{extended_start_script, true}." \
-	"{sys_config, \"rel/sys.config\"}." \
-	"{vm_args, \"rel/vm.args\"}."
-bs_sys_config = "[" \
-	"]."
-bs_vm_args = "-name $(PROJECT)@127.0.0.1" \
-	"-setcookie $(PROJECT)" \
-	"-heart"
-# Normal templates.
-tpl_supervisor = "-module($(n))." \
-	"-behaviour(supervisor)." \
-	"" \
-	"-export([start_link/0])." \
-	"-export([init/1])." \
-	"" \
-	"start_link() ->" \
-	"	supervisor:start_link({local, ?MODULE}, ?MODULE, [])." \
-	"" \
-	"init([]) ->" \
-	"	Procs = []," \
-	"	{ok, {{one_for_one, 1, 5}, Procs}}."
-tpl_gen_server = "-module($(n))." \
-	"-behaviour(gen_server)." \
-	"" \
-	"%% API." \
-	"-export([start_link/0])." \
-	"" \
-	"%% gen_server." \
-	"-export([init/1])." \
-	"-export([handle_call/3])." \
-	"-export([handle_cast/2])." \
-	"-export([handle_info/2])." \
-	"-export([terminate/2])." \
-	"-export([code_change/3])." \
-	"" \
-	"-record(state, {" \
-	"})." \
-	"" \
-	"%% API." \
-	"" \
-	"-spec start_link() -> {ok, pid()}." \
-	"start_link() ->" \
-	"	gen_server:start_link(?MODULE, [], [])." \
-	"" \
-	"%% gen_server." \
-	"" \
-	"init([]) ->" \
-	"	{ok, \#state{}}." \
-	"" \
-	"handle_call(_Request, _From, State) ->" \
-	"	{reply, ignored, State}." \
-	"" \
-	"handle_cast(_Msg, State) ->" \
-	"	{noreply, State}." \
-	"" \
-	"handle_info(_Info, State) ->" \
-	"	{noreply, State}." \
-	"" \
-	"terminate(_Reason, _State) ->" \
-	"	ok." \
-	"" \
-	"code_change(_OldVsn, State, _Extra) ->" \
-	"	{ok, State}."
-tpl_cowboy_http = "-module($(n))." \
-	"-behaviour(cowboy_http_handler)." \
-	"" \
-	"-export([init/3])." \
-	"-export([handle/2])." \
-	"-export([terminate/3])." \
-	"" \
-	"-record(state, {" \
-	"})." \
-	"" \
-	"init(_, Req, _Opts) ->" \
-	"	{ok, Req, \#state{}}." \
-	"" \
-	"handle(Req, State=\#state{}) ->" \
-	"	{ok, Req2} = cowboy_req:reply(200, Req)," \
-	"	{ok, Req2, State}." \
-	"" \
-	"terminate(_Reason, _Req, _State) ->" \
-	"	ok."
-tpl_cowboy_loop = "-module($(n))." \
-	"-behaviour(cowboy_loop_handler)." \
-	"" \
-	"-export([init/3])." \
-	"-export([info/3])." \
-	"-export([terminate/3])." \
-	"" \
-	"-record(state, {" \
-	"})." \
-	"" \
-	"init(_, Req, _Opts) ->" \
-	"	{loop, Req, \#state{}, 5000, hibernate}." \
-	"" \
-	"info(_Info, Req, State) ->" \
-	"	{loop, Req, State, hibernate}." \
-	"" \
-	"terminate(_Reason, _Req, _State) ->" \
-	"	ok."
-tpl_cowboy_rest = "-module($(n))." \
-	"" \
-	"-export([init/3])." \
-	"-export([content_types_provided/2])." \
-	"-export([get_html/2])." \
-	"" \
-	"init(_, _Req, _Opts) ->" \
-	"	{upgrade, protocol, cowboy_rest}." \
-	"" \
-	"content_types_provided(Req, State) ->" \
-	"	{[{{<<\"text\">>, <<\"html\">>, '_'}, get_html}], Req, State}." \
-	"" \
-	"get_html(Req, State) ->" \
-	"	{<<\"<html><body>This is REST!</body></html>\">>, Req, State}."
-tpl_cowboy_ws = "-module($(n))." \
-	"-behaviour(cowboy_websocket_handler)." \
-	"" \
-	"-export([init/3])." \
-	"-export([websocket_init/3])." \
-	"-export([websocket_handle/3])." \
-	"-export([websocket_info/3])." \
-	"-export([websocket_terminate/3])." \
-	"" \
-	"-record(state, {" \
-	"})." \
-	"" \
-	"init(_, _, _) ->" \
-	"	{upgrade, protocol, cowboy_websocket}." \
-	"" \
-	"websocket_init(_, Req, _Opts) ->" \
-	"	Req2 = cowboy_req:compact(Req)," \
-	"	{ok, Req2, \#state{}}." \
-	"" \
-	"websocket_handle({text, Data}, Req, State) ->" \
-	"	{reply, {text, Data}, Req, State};" \
-	"websocket_handle({binary, Data}, Req, State) ->" \
-	"	{reply, {binary, Data}, Req, State};" \
-	"websocket_handle(_Frame, Req, State) ->" \
-	"	{ok, Req, State}." \
-	"" \
-	"websocket_info(_Info, Req, State) ->" \
-	"	{ok, Req, State}." \
-	"" \
-	"websocket_terminate(_Reason, _Req, _State) ->" \
-	"	ok."
-tpl_ranch_protocol = "-module($(n))." \
-	"-behaviour(ranch_protocol)." \
-	"" \
-	"-export([start_link/4])." \
-	"-export([init/4])." \
-	"" \
-	"-type opts() :: []." \
-	"-export_type([opts/0])." \
-	"" \
-	"-record(state, {" \
-	"	socket :: inet:socket()," \
-	"	transport :: module()" \
-	"})." \
-	"" \
-	"start_link(Ref, Socket, Transport, Opts) ->" \
-	"	Pid = spawn_link(?MODULE, init, [Ref, Socket, Transport, Opts])," \
-	"	{ok, Pid}." \
-	"" \
-	"-spec init(ranch:ref(), inet:socket(), module(), opts()) -> ok." \
-	"init(Ref, Socket, Transport, _Opts) ->" \
-	"	ok = ranch:accept_ack(Ref)," \
-	"	loop(\#state{socket=Socket, transport=Transport})." \
-	"" \
-	"loop(State) ->" \
-	"	loop(State)."
-
-# Plugin-specific targets.
-
-bootstrap:
-ifneq ($(wildcard src/),)
-	$(error Error: src/ directory already exists)
-endif
-	@printf "%s\n" $(bs_Makefile) > Makefile
-	@mkdir src/
-	@printf "%s\n" $(bs_appsrc) > src/$(PROJECT).app.src
-	@printf "%s\n" $(bs_app) > src/$(PROJECT)_app.erl
-	$(eval n := $(PROJECT)_sup)
-	@printf "%s\n" $(tpl_supervisor) > src/$(PROJECT)_sup.erl
-
-bootstrap-lib:
-ifneq ($(wildcard src/),)
-	$(error Error: src/ directory already exists)
-endif
-	@printf "%s\n" $(bs_Makefile) > Makefile
-	@mkdir src/
-	@printf "%s\n" $(bs_appsrc_lib) > src/$(PROJECT).app.src
-
-bootstrap-rel:
-ifneq ($(wildcard relx.config),)
-	$(error Error: relx.config already exists)
-endif
-ifneq ($(wildcard rel/),)
-	$(error Error: rel/ directory already exists)
-endif
-	@printf "%s\n" $(bs_relx_config) > relx.config
-	@mkdir rel/
-	@printf "%s\n" $(bs_sys_config) > rel/sys.config
-	@printf "%s\n" $(bs_vm_args) > rel/vm.args
-
-new:
-ifeq ($(wildcard src/),)
-	$(error Error: src/ directory does not exist)
-endif
-ifndef t
-	$(error Usage: make new t=TEMPLATE n=NAME)
-endif
-ifndef tpl_$(t)
-	$(error Unknown template)
-endif
-ifndef n
-	$(error Usage: make new t=TEMPLATE n=NAME)
-endif
-	@printf "%s\n" $(tpl_$(t)) > src/$(n).erl
-
-list-templates:
-	@echo Available templates: $(sort $(patsubst tpl_%,%,$(filter tpl_%,$(.VARIABLES))))
 
 # Copyright (c) 2013-2014, Loïc Hoguin <essen@ninenines.eu>
 # This file is part of erlang.mk and subject to the terms of the ISC License.
@@ -618,40 +352,20 @@ help::
 
 # Plugin-specific targets.
 
-plt: deps app
+$(DIALYZER_PLT): deps app
 	@dialyzer --build_plt --apps erts kernel stdlib $(PLT_APPS) $(ALL_DEPS_DIRS)
+
+plt: $(DIALYZER_PLT)
 
 distclean-plt:
 	$(gen_verbose) rm -f $(DIALYZER_PLT)
 
+ifneq ($(wildcard $(DIALYZER_PLT)),)
 dialyze:
-	@dialyzer --no_native --src -r src $(DIALYZER_OPTS)
-
-# Copyright (c) 2013-2014, Loïc Hoguin <essen@ninenines.eu>
-# This file is part of erlang.mk and subject to the terms of the ISC License.
-
-# Verbosity.
-
-dtl_verbose_0 = @echo " DTL   " $(filter %.dtl,$(?F));
-dtl_verbose = $(dtl_verbose_$(V))
-
-# Core targets.
-
-define compile_erlydtl
-	$(dtl_verbose) erl -noshell -pa ebin/ $(DEPS_DIR)/erlydtl/ebin/ -eval ' \
-		Compile = fun(F) -> \
-			Module = list_to_atom( \
-				string:to_lower(filename:basename(F, ".dtl")) ++ "_dtl"), \
-			erlydtl:compile(F, Module, [{out_dir, "ebin/"}]) \
-		end, \
-		_ = [Compile(F) || F <- string:tokens("$(1)", " ")], \
-		init:stop()'
-endef
-
-ifneq ($(wildcard src/),)
-ebin/$(PROJECT).app:: $(shell find templates -type f -name \*.dtl 2>/dev/null)
-	$(if $(strip $?),$(call compile_erlydtl,$?))
+else
+dialyze: $(DIALYZER_PLT)
 endif
+	@dialyzer --no_native --src -r src $(DIALYZER_OPTS)
 
 # Copyright (c) 2013-2014, Loïc Hoguin <essen@ninenines.eu>
 # This file is part of erlang.mk and subject to the terms of the ISC License.
@@ -674,47 +388,3 @@ distclean:: distclean-edoc
 
 distclean-edoc:
 	$(gen_verbose) rm -f doc/*.css doc/*.html doc/*.png doc/edoc-info
-
-# Copyright (c) 2013-2014, Loïc Hoguin <essen@ninenines.eu>
-# This file is part of erlang.mk and subject to the terms of the ISC License.
-
-.PHONY: distclean-rel
-
-# Configuration.
-
-RELX_CONFIG ?= $(CURDIR)/relx.config
-
-ifneq ($(wildcard $(RELX_CONFIG)),)
-
-RELX ?= $(CURDIR)/relx
-export RELX
-
-RELX_URL ?= https://github.com/erlware/relx/releases/download/v1.0.2/relx
-RELX_OPTS ?=
-RELX_OUTPUT_DIR ?= _rel
-
-ifeq ($(firstword $(RELX_OPTS)),-o)
-	RELX_OUTPUT_DIR = $(word 2,$(RELX_OPTS))
-endif
-
-# Core targets.
-
-rel:: distclean-rel $(RELX)
-	@$(RELX) -c $(RELX_CONFIG) $(RELX_OPTS)
-
-distclean:: distclean-rel
-
-# Plugin-specific targets.
-
-define relx_fetch
-	$(call core_http_get,$(RELX),$(RELX_URL))
-	chmod +x $(RELX)
-endef
-
-$(RELX):
-	@$(call relx_fetch)
-
-distclean-rel:
-	$(gen_verbose) rm -rf $(RELX) $(RELX_OUTPUT_DIR)
-
-endif
